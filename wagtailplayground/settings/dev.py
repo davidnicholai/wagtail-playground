e57780b9cefb520c783aa526401dc24a01976f65
David Lim <nicholai.david@gmail.com>
2022-05-22 09:24:44 +0800

    Initial commit

---
+++ wagtailplayground/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-2+-i59bqcf1_ha0up_m5d1yr6c#dz8y1u60=9uc0-7itbg)imf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
